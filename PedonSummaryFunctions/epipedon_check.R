4b05b44e5d57e307574bf9a13a0c5bd7f385e111
Andrew Brown <andrew.g.brown@ca.usda.gov>
2017-08-11 10:22:23 -0700

    clean up PedonSummaryFunctions

---
+++ PedonSummaryFunctions/epipedon_check.R
@@ -0,0 +1,29 @@
+source("PedonSummaryFunctions\\pedon_summary_functions.R")
+
+#get the data
+library(soilDB)
+f <- fetchNASIS()
+foo <- diagnostic_hz(f)
+
+#calc thickness
+foo$thickness <- foo$featdepb-foo$featdept
+
+#filter to the dark (or nondark) epipedons for Pedon Diagnostic Horizon table
+foo2 <- foo[grepl(foo$diag_kind,pat="(ochric|mollic|umbric) epipedon"),]
+foo3 <- data.frame(peiid=foo2$peiid, epipedon_thickness=foo2$thickness)
+site(f) <- merge(site(f), foo3, by="peiid")
+
+#use hasDarkMineralSurface() to check whether the diagnostics table match the data in the horizon table
+has_epipedon <- do.call(rbind,profileApply(f,FUN=hasDarkMineralSurface, bounds=T, remove.na=T, simplify=F))
+
+#merge and check for matches of top and bottom depth
+has_umbric_data$peiid <- rownames(has_epipedon)
+foo4 <- merge(foo2, has_epipedon, by="peiid")
+foo4$tmatch <- foo4$featdept == foo4$dsubound
+foo4$bmatch <- foo4$featdepb == foo4$dslbound
+foo4$match <-  foo4$tmatch & foo4$bmatch
+
+#write output
+write.csv(foo4,file="PedonSummaryFunctions\\dark_epipedon_check.csv")
+
+#p<-f[which(site(f)$peiid==1193379),]
