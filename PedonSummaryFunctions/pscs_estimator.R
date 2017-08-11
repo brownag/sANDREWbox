200584488a93fe6e25e89463c6e08d4e6371a21e
Andrew Brown <andrew.g.brown@ca.usda.gov>
2017-08-11 10:15:49 -0700

    formatting pscs estimator

--- PedonSummaryFunctions/pscs_estimator.R
+++ PedonSummaryFunctions/pscs_estimator.R
@@ -1,15 +1,18 @@
 source("pedon_summary_functions.R")
+
+#get the data
 library(soilDB)
-get_extended_data_from_NASIS_db
 pedons = fetchNASIS()
 
+#checks
 #c1=profileApply(pedons, getPlowLayerDepth)
 #c2=profileApply(pedons, estimateRootingDepth)
 
-pscsraw = profileApply(pedons, estimatePSCS) #get pscs bounds
+##get pscs bounds
+pscsraw = profileApply(pedons, estimatePSCS) 
 
 #make output
 lo = as.logical(1:length(pscsraw) %% 2)
 pscs_top = pscsraw[lo]
 pscs_bot = pscsraw[!lo]
-write.csv(transform(data.frame(pedons$site_id,pedons$taxonname,pedons$psctopdepth,pedons$pscbotdepth,esttop=pscs_top,estbot=pscs_bot, tmatch=(pedons$psctopdepth == pscs_top), bmatch=(pedons$pscbotdepth == pscs_bot)),match=(tmatch==bmatch & tmatch == T)),"CA630_pscs_check.csv")
+write.csv(transform(data.frame(pedons$site_id,pedons$taxonname,pedons$psctopdepth,pedons$pscbotdepth,esttop=pscs_top,estbot=pscs_bot, tmatch=(pedons$psctopdepth == pscs_top), bmatch=(pedons$pscbotdepth == pscs_bot)),match=(tmatch==bmatch & tmatch == T)), "CA630_pscs_check.csv")
