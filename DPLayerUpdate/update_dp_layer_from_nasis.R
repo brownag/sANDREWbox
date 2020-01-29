890d0564b217dc3db81e5e8031aa9853d1a7d0cf
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-01-29 11:52:36 -0800

    modifications for integrating site data from sites without pedons

--- DPLayerUpdate/update_dp_layer_from_nasis.R
+++ DPLayerUpdate/update_dp_layer_from_nasis.R
@@ -1,25 +1,32 @@
 library(aqp)
 library(soilDB)
 library(rgdal)
+
 f <- fetchNASIS()
-n <- site(f)#get_site_data_from_NASIS_db()
+n <- site(f)
+
+# merging can and will break down without this option
+options(stringsAsFactors = FALSE)
+
 setwd('S:/NRCS/Archive_Andrew_Brown/CA649/Points')
-dp <- readOGR(dsn='.', layer="ca649_dp")
+dp <- readOGR(dsn='.', layer="ca649_dp", stringsAsFactors = FALSE)
 ident.var <- 'ident'
-#get the low hanging fruit. direct matches.
-dp.new <- merge(dp, n, by.y='pedon_id', by.x=ident.var, all.x=T)
 
-#try removing Ns
-dp$IDENT_noN <- ""
-dp$IDENT_noN <- gsub(dp[[ident.var]], pattern = "N", replacement = "")
-if(length(is.na(dp.new$taxonname))) {
-  note_taxa <- merge(dp[is.na(dp.new$taxonname), ], n, by.y='pedon_id', by.x='IDENT_noN', all.x=T)$taxonname
-  dp.new$taxonname[is.na(dp.new$taxonname)] <- note_taxa
-}
-nrow(dp.new)
+# direct matches, with pedon associated with site record
+dp.new <- merge(dp[,c(ident.var, 'altitude')], n, by.y='pedon_id', by.x=ident.var, sort=F)
 
-writeOGR(dp.new[, c("ident", "type", "Latitude", "Longitude", "y_proj", "x_proj", "comment", 
-                    "altitude", "filename", "site_id", "taxonname", "taxonkind", "taxpartsize","ecositeid", "pedonpurpose", "pedontype")], 
+siterecs <- get_site_data_from_NASIS_db()[,c('siteiid','site_id')]
+eco.history <- get_extended_data_from_NASIS_db()$ecositehistory
+ecosite <- do.call('rbind', lapply(split(eco.history, f=eco.history$siteiid), soilDB:::.pickBestEcosite))
+
+ecosite.lut <- as.character(ecosite$ecositeid)
+names(ecosite.lut) <- merge(ecosite, siterecs, sort=F, all.x=T)$site_id
+
+no.ecosite.idx <- which(is.na(dp.new$ecositeid))
+dp.new[no.ecosite.idx, 'ecositeid']<- as.character(ecosite.lut[dp.new[no.ecosite.idx,]$ident])
+
+writeOGR(dp.new, 
          dsn=".",
          layer="updated_ca649_dp",
-         driver="ESRI Shapefile")
+         driver="ESRI Shapefile",
+         overwrite_layer = TRUE)
