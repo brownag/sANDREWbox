21d8061d7a381858ee910bd2b95fd6fcef618cbf
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-03-24 11:45:19 -0700

    add lattice plots of aqp::slab'd geochem by analyte+MLRA

--- KSSLExtended/fetchKSSL_heavymetals_demo.R
+++ KSSLExtended/fetchKSSL_heavymetals_demo.R
@@ -34,6 +34,9 @@ keep.idx <- do.call('c', lapply(element.list, function(element) {
   }))
 geochem<- geochem[, c(1:3,keep.idx)]
 
+# transfer geochem to horizon table (note that labsampnum is commonly nonunique)
+horizons(spc) <- unique(geochem)
+
 # transfer mlra labels to geochem table
 spc$hzmlra <- denormalize(spc, 'mlra')
 h <- horizons(spc)
@@ -44,3 +47,26 @@ lapply(as.list(colnames(geochem[,-c(1:3, length(colnames(geochem)))])), function
   print(sprintf("%s ~ mlra", element))
   boxplot(data=geochem, as.formula(sprintf("%s ~ mlra", element)), xlab="MLRA")
 })
+
+# slab-wise plots 
+
+# slab by depth.interval
+depth.interval <- 10
+res <- slab(spc, fm = mlra ~ lead_trace_element + mercury_trace_element, slab.structure = depth.interval)
+
+library(lattice)
+for(m in mlras) {
+  res.sub <- res[res$mlra == m,]
+  print(xyplot(top ~ p.q50 | variable,  data=res.sub, 
+               ylab='Depth',
+               xlab=paste0('median bounded by 25th and 75th percentiles (MLRA: ', m,')'),
+               lower=res.sub$p.q25, upper=res.sub$p.q75, ylim=c(200,-2),
+               panel=panel.depth_function,
+               alpha=0.25, sync.colors=TRUE,
+               par.settings=list(superpose.line=list(col='RoyalBlue', lwd=2)),
+               prepanel=prepanel.depth_function,
+               cf=res.sub$contributing_fraction, cf.col='black', cf.interval=depth.interval, 
+               layout=c(2,1), strip=strip.custom(bg=grey(0.8)),
+               scales=list(x=list(tick.number=4, alternating=3, relation='free'))
+  ))
+}
