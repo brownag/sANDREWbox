ea65b8202b9c8d6f2bb041f435df68682219893b
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-08-18 12:34:37 -0700

    fix slab aggregation of standard deviations

--- Tidy/group_by-summarize_vs_slab.R
+++ Tidy/group_by-summarize_vs_slab.R
@@ -12,8 +12,10 @@ kssltrunc <- trunc(kssldat, 0, 30)
 # new way to do it: group_by + summarize
 ksslgroup <- group_by(kssltrunc, "mlra") 
 
+# profile identity split
+# ksslgroup <- group_by(kssltrunc, idname(kssltrunc)) 
+
 # summarize takes one or more expressions that resolve to 1 value per group
-#   default grouping is single profiles; based on profile_id (without a group_by call)
 res1 <- summarize(ksslgroup, 
                   oc_mean = mean(oc, na.rm = TRUE), 
                   ph_h2o_mean = mean(ph_h2o, na.rm = TRUE),
@@ -22,14 +24,19 @@ res1 <- summarize(ksslgroup,
 
 # slab way to do it; using 0-30cm trunc'd SPC
 slabres  <- aqp::slab(ksslgroup, mlra ~ oc + estimated_oc + estimated_om + ph_h2o, 
-                      slab.fun = mean, na.rm = TRUE)
+                      slab.fun = function(x) {
+                        foo <- c(mean(x, na.rm = TRUE), 
+                                 sd(x, na.rm = TRUE))
+                        names(foo) <- c("Mean","SD")
+                        return(foo)
+                      })
 
 res2 <- do.call('rbind', lapply(split(slabres, f = slabres$mlra), function(mlra) {
   data.frame(mlra = unique(mlra$mlra),
-             oc_mean = mean(subset(mlra, variable == "oc")$value, na.rm = TRUE), 
-             ph_h2o_mean = mean(subset(mlra, variable == "ph_h2o")$value, na.rm = TRUE),
-             oc_sd = sd(subset(mlra, variable == "oc")$value, na.rm = TRUE), 
-             ph_h2o_sd = sd(subset(mlra, variable == "ph_h2o")$value, na.rm = TRUE))
+             oc_mean = mean(subset(mlra, variable == "oc")$Mean, na.rm = TRUE), 
+             ph_h2o_mean = mean(subset(mlra, variable == "ph_h2o")$Mean, na.rm = TRUE),
+             oc_sd = mean(subset(mlra, variable == "oc")$SD, na.rm = TRUE), 
+             ph_h2o_sd = mean(subset(mlra, variable == "ph_h2o")$SD, na.rm = TRUE))
 }))
 
 # this is using un-sliced data
@@ -38,8 +45,8 @@ res1
 #1   18 1.748223    6.097802 3.800573 0.5514775
 #2  22A 2.970130    5.700705 3.315585 0.5555779
 
-# this is using sliced data -- similar mean values, but sd reflects slice-depth variation
+# this is using sliced data -- similar values
 res2
-#    mlra  oc_mean ph_h2o_mean     oc_sd  ph_h2o_sd
-#18    18 1.682727    6.090462 0.5516752 0.04559616
-#22A  22A 2.972001    5.696293 0.8850780 0.01405011
+# mlra  oc_mean ph_h2o_mean    oc_sd ph_h2o_sd
+# 18    18 1.682727    6.090462 4.044607 0.5393427
+# 22A  22A 2.972001    5.696293 3.167955 0.5505772
