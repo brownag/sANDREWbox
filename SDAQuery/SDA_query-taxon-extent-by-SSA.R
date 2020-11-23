b0098adcbc249c865bfc45c71e3ed44e8d8f4639
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-11-23 10:33:29 -0800

    :art: cleanup

--- SDAQuery/SDA_query-taxon-extent-by-SSA.R
+++ SDAQuery/SDA_query-taxon-extent-by-SSA.R
@@ -12,10 +12,10 @@ taxonomic_level <- "Ultic Haploxeralfs"
 group_level <- "taxsubgrp"
 majors_only <- TRUE
 
-# get compoent data
+# get component data
 component_data <- fetchSDA(sprintf("areasymbol = '%s'", soil_survey_area))
 
-component_data <- subset(component_data, !is.na(component_data[[group_level]])) ## NOTE: taxsubgrp hardcoded
+component_data <- subset(component_data, !is.na(component_data[[group_level]])) 
 
 # get spatial data (based on nmusyms in component data)
 spatial_data <- st_as_sf(fetchSDA_spatial(component_data$nationalmusym, by.col = "nmusym"))
@@ -25,7 +25,8 @@ if (majors_only) {
   major_component_data <- subset(component_data, majcompflag == "Yes")
 } else major_component_data <- component_data
 
-subgrps <- split(major_component_data, group_level) ## NOTE: taxsubgrp hardcoded
+subgrps <- split(major_component_data, group_level)
+
 #' Cross-reference national musym for SPC and spatial data to produce spatial subsets
 #'
 #' @param spc A SoilProfileCollection
@@ -40,7 +41,7 @@ get_poly_from_SPC <- function(spc, spat) {
 # make extent map of all Ultic Haploxeralfs
 display_extent <- get_poly_from_SPC(subgrps[[taxonomic_level]], spatial_data)
 
-## Example graphic:
+## --- Example graphic 1 ---
 
 # create a subset of the soils with temp regime mesic
 mesic_ulha <- subset(subgrps[[taxonomic_level]], 
@@ -55,6 +56,8 @@ ggplot() +
   geom_sf(data = mesic_display_extent, aes(fill = "Mesic"), color=NA) +
   annotation_north_arrow(which_north = "true") +
   scale_fill_manual(name = "Ultic Haploxeralfs", values = c("Thermic" = "red", "Mesic" = "blue"))
+
+## --- Example graphic 2 ---
 
 # compare to full extent of ultic haploxeralfs derived from soil web
 library(raster)
